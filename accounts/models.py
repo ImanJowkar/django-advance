43cfcd721a6bf7eec5b3dfc556aee7072da3c74a
ImanJowkar <77024269+ImanJowkar@users.noreply.github.com>
2023-08-25 12:48:33 +0330

    compelete class based view

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, Permi
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 # Create your models here.
 
 class UserManager(BaseUserManager):
